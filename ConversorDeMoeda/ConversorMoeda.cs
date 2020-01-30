4ae0208ed86a7672df542642b93871edfb91cff0
Victor Santos <vhos08@gmail.com>
2020-01-30 16:49:18 -0300

    Membros Statics 1

---
+++ ConversorDeMoeda/ConversorMoeda.cs
@@ -0,0 +1,14 @@
+﻿namespace ConversorDeMoeda
+{
+    class ConversorMoeda
+    {
+
+        public static double IOF = 0.06;
+
+        public static double DolarReais(double cotacaoDolar, double valorCompra)
+        {
+            return (valorCompra + (valorCompra * IOF)) * cotacaoDolar;
+        }
+
+    }
+}
---
+++ ConversorDeMoeda/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConversorDeMoeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do Dolar? ");
+            double cotacaoDolar = Double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double valorCompra = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor a ser pago em reais = " + ConversorMoeda.DolarReais(cotacaoDolar,valorCompra).ToString("F2"));
+        }
+    }
+}
